613d60654b12660e19ff95c869ef46bfa8aef519
Paul Cooke <paulgfcooke@gmail.com>
2019-10-24 19:54:30 +0100

    render page from totally empty app

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import './styles/style.scss'
+
+//import Navbar from './components/common/Navbar'
+
+const App = () => (
+  
+  <h1>render app</h1>
+  
+)
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
