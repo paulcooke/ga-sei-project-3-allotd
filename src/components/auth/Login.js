bf3a2c6053051c0ebea99c06061d9f1ce101e80d
lloydnoone <lloydnoone92@gmail.com>
2019-10-25 13:49:59 +0100

    new form

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,15 +1,58 @@
 import React from 'react'
-// remember to import auth
-// remember to import axios
+import axios from 'axios'
+
+import Auth from '../../lib/auth'
 
 class Login extends React.Component {
   constructor() {
     super()
+    
+    this.state = {
+      data: {
+        email: '',
+        password: ''
+      }
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange({ target: { name, value } }) {
+    const data = { ...this.state.data, [name]: value }
+    this.setState({ data })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    axios.post('/api/login', this.state.data )
+      .then(res => {
+        Auth.setToken(res.data.token)
+        this.props.history.push('/cigars')
+      })
+      .catch(err => console.log(err.message))
   }
 
   render() {
     return (
-      <h1>check me out im the login page</h1>
+      <div className='formWrapper'>
+        <form className='panelWrapper' onSubmit={this.handleSubmit}>
+          <h2>Login</h2>
+          <label>Email</label>
+          <input
+            name='email'
+            placeholder='name@email.com'
+            onChange={this.handleChange}
+          />
+          <label>Password</label>
+          <input
+            name='password'
+            type='password'
+            placeholder='Password'
+            onChange={this.handleChange}
+          />
+          <button type='submit'>Login</button>
+        </form>
+      </div>
     )
   }
 }
--- src/components/vegetables/VegetablesNew.js
+++ src/components/vegetables/VegetablesNew.js
@@ -1,17 +1,112 @@
 import React from 'react'
 import VegetablesForm from './VegetablesForm'
+import axios from 'axios'
+
+import Auth from '../../lib/auth'
+import { type } from 'os'
 
 class VegetablesNew extends React.Component {
   constructor() {
     super()
+
+    this.state = {
+      data: {
+        title: '', // { type: String, required: true },
+        typeOfVeg: '', // { type: String, required: true },
+        varietyOfVeg: '', //{ type: String },
+        pickedDate: '', //{ type: Date, required: true },
+        description: '', // { type: String, maxlength: 200 },
+        image: '', //{ type: String },
+        vegLocation: '' //{ type: String, required: true },
+      }
+    }
+    this.handleChange = this.handleChange.bind(this)
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleChange(e) {
+    const data = { ...this.state.data, [e.target.name]: e.target.value }
+    //make errors blank initially
+    const errors = { ...this.state.errors, [e.target.name]: '' }
+    this.setState({ data, errors })
+  }
+
+  handleSubmit(e) {
+    e.preventDefault()
+    axios.post('/api/vegetables', this.state.data, {
+      headers: { Authorization: `Bearer ${Auth.getToken()}` }
+    })
+      .then(res => this.props.history.push(`/vegetables/${res.data._id}`))
+      .catch(err => this.setState({ errors: err.message }))
   }
 
   render() {
+    // return (
+    //   <>
+    //     <h1>Make your veggies here! on the CREATE PAGE!!!</h1>
+    //     <VegetablesForm />
+    const { data: { title, typeOfVeg, varietyOfVeg, pickedDate, description, image, vegLocation } } = this.state
     return (
-      <>
-        <h1>Make your veggies here! on the CREATE PAGE!!!</h1>
-        <VegetablesForm />
-      </>
+      <div className='formWrapper'>
+        <form className='panelWrapper' onSubmit={this.handleSubmit}>
+          <h2>New Vegetable</h2>
+          <label>Name</label>
+          <input
+            placeholder="Name"
+            name="title"
+            onChange={this.handleChange}
+            value={title}
+          />
+          <label>Type</label>
+          <input
+            placeholder="Type"
+            name="typeOfVeg"
+            onChange={this.handleChange}
+            value={typeOfVeg}
+          />
+          <label>Variety</label>
+          <input
+            placeholder="Variety"
+            name="varietyOfVeg"
+            onChange={this.handleChange}
+            value={varietyOfVeg}
+          />
+          <label>Date Picked</label>
+          <input
+            placeholder="Date Picked"
+            name="pickedDate"
+            onChange={this.handleChange}
+            value={pickedDate}
+          />
+          <label>Image URL</label>
+          <input
+            placeholder='Image URL'
+            name='image'
+            onChange={this.handleChange}
+            value={image}
+          />
+          <label>Location</label>
+          <input
+            placeholder='Postcode'
+            name='vegLocation'
+            onChange={this.handleChange}
+            value={vegLocation}
+          />
+          <label>Description</label>
+          <textarea
+            rows='4'
+            cols='5'
+            type='textarea'
+            placeholder="Description"
+            name="description"
+            onChange={this.handleChange}
+            value={description}
+          />
+          <button type="submit">
+            submit
+          </button>
+        </form>
+      </div>
     )
   }
 }
