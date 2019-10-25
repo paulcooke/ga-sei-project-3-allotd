aced2e722de346c7d4914a0fd49cd0dff51d2378
Claire <smith.clairelouise@gmail.com>
2019-10-25 15:43:47 +0100

    dashboard framework complete

--- src/components/common/Dashboard.js
+++ src/components/common/Dashboard.js
@@ -14,9 +14,11 @@ class Dashboard extends React.Component {
         email: '',
         userImage: '',
         userLocation: '',
-        vegGrown: '',
-        vegLookingFor: '',
-        rating: ''
+        vegGrown: [],
+        vegLookingFor: [],
+        rating: '',
+        availablePickUpDays: [],
+        availablePickUpTimes: []
       }
     }
     // bind here
@@ -26,13 +28,56 @@ class Dashboard extends React.Component {
     axios.get('/api/profile', {
       headers: { Authorization: `Bearer ${Auth.getToken()}` }
     })
-      .then(res => console.log(res.data))
+      .then(res => this.setState({ data: res.data }))
       .catch(err => console.log(err.message))
   }
 
   render() {
+    console.log(this.state)
+    
     return (
-      <h1>Hello</h1>
+      <main>
+        <section className='panelWrapper'>
+          <div>
+            <h1>Dashboard</h1>
+          </div>
+          <div>
+            <h2>Username: {this.state.data.username}</h2>
+            <h3>User Rating {this.state.data.rating}: </h3>
+          </div>
+          <div>
+            <h2>Grown by me</h2>
+            {this.state.data.vegGrown.map(veg => 
+              <p key={veg}>{veg}</p>
+            )}
+          </div>
+          <div>
+            <h2>Veggies I like</h2>
+            {this.state.data.vegLookingFor.map(veg => 
+              <p key={veg}>{veg}</p>
+            )}
+          </div>
+          <div>
+            <h2>My availability</h2>
+            {this.state.data.availablePickUpDays.map(veg => 
+              <p key={veg}>{veg}</p>
+            )}
+          </div>
+          <div>
+            {this.state.data.availablePickUpTimes.map(veg => 
+              <p key={veg}>{veg}</p>
+            )}
+          </div>
+        </section>
+        <section>
+          <div className='panelWrapper'>
+       My Listings
+          </div>
+          <div className='panelWrapper'>
+       My claimed veggies
+          </div>
+        </section>
+      </main>
     )
   }
 
