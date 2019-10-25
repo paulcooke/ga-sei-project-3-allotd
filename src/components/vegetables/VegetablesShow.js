455d096d7e58029f943426e9235c09b8f3b89436
lloydnoone <lloydnoone92@gmail.com>
2019-10-25 18:30:10 +0100

    showpage

--- src/components/vegetables/VegetablesShow.js
+++ src/components/vegetables/VegetablesShow.js
@@ -1,19 +1,74 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-// might need axios in here to post images to be saved somewhere?
+import Auth from '../../lib/auth'
+import axios from 'axios'
 
 
 class VegetablesShow extends React.Component {
   constructor() {
     super()
+
+    this.state = {
+      vegetable: null
+    }
+    this.handleDelete = this.handleDelete.bind(this)
+  }
+
+  componentDidMount() {
+    const vegId = this.props.match.params.id //only need this reference once
+    axios.get(`/api/vegetables/${vegId}`)
+      .then(res => this.setState({ vegetable: res.data }))
+      .catch(err => console.log(err))
+  }
+
+  handleDelete() {
+    const vegetableId = this.props.match.params.id
+    axios.delete(`/api/vegetables/${vegetableId}`, {
+      headers: { Authorization: `Bearer ${Auth.getToken()}` }
+    })
+      .then(() => this.props.history.push('/vegetables'))
+      .catch(err => console.log(err))
+  }
+
+  isOwner() {
+    return Auth.getPayload().sub === this.state.vegetable.user._id
   }
 
   render() {
+    if (!this.state.vegetable) return null
+    const { image, title, typeOfVeg, varietyOfVeg, pickedDate, description, isClaimed,
+      vegLocation, availablePickUpDays, availablePickUpTimes, user
+    } = this.state.vegetable
     return (
-      <>
-        <h1>showing off a veggie on the SHOW PAGE!!!</h1>
-        <Link to="/vegetables/:id/edit">This link is for the edit button to the edit page</Link>
-      </>
+      //<Link to="/vegetables/:id/edit">This link is for the edit button to the edit page</Link>
+      <div className='showWrapper'>
+        <div className='imgAndInfo'>
+          <img src={image} alt={title} />
+          <div className='panelWrapper'>
+            <div>
+              <h1>{title}</h1>
+              <p>Type: {typeOfVeg}</p>
+              <p>Variety: {varietyOfVeg}</p>
+              <p>Date Picked: {pickedDate}</p>
+              <p>Description: {description}</p>
+              <p>Claimed: {isClaimed}</p>
+              <p>vegLocation: {vegLocation}</p>
+              <p>Available on {availablePickUpDays} at {availablePickUpTimes}</p>
+              <p>Posted by: {user.username}</p>
+            </div>
+            <div>
+              {this.isOwner() &&
+                <>
+                  <Link to={`/cigars/${this.state.vegetable._id}/edit`}>
+                    <button>Edit vegetable</button>
+                  </Link>
+                  <button onClick={this.handleDelete}>Delete vegetable</button>
+                </>
+              }
+            </div>
+          </div>
+        </div>
+      </div>
     )
   }
 }
