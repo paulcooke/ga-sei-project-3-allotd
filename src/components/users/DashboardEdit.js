91943afba5a24aa40168bd783dc61fce6aae029e
Paul Cooke <paulgfcooke@gmail.com>
2019-10-27 19:17:31 +0000

    small fixes

--- src/components/users/DashboardEdit.js
+++ src/components/users/DashboardEdit.js
@@ -43,17 +43,17 @@ class DashboardEdit extends React.Component {
       { value: '9', label: '9:00' },
       { value: '10', label: '10:00' },
       { value: '11', label: '11:00' },
-      { value: '12', label: 'noon' },
-      { value: '13', label: '1:00' },
-      { value: '14', label: '2:00' },
-      { value: '15', label: '3:00' },
-      { value: '16', label: '4:00' },
-      { value: '17', label: '5:00' },
-      { value: '18', label: '6:00' },
-      { value: '19', label: '7:00' },
-      { value: '20', label: '8:00' },
-      { value: '21', label: '9:00' },
-      { value: '22', label: '10:00' }
+      { value: '12', label: '12:00' },
+      { value: '13', label: '13:00' },
+      { value: '14', label: '14:00' },
+      { value: '15', label: '15:00' },
+      { value: '16', label: '16:00' },
+      { value: '17', label: '17:00' },
+      { value: '18', label: '18:00' },
+      { value: '19', label: '19:00' },
+      { value: '20', label: '20:00' },
+      { value: '21', label: '21:00' },
+      { value: '22', label: '22:00' }
     ]
 
     this.handleChange = this.handleChange.bind(this)
@@ -162,6 +162,11 @@ class DashboardEdit extends React.Component {
               isMulti
               onChange={this.handleDaySelect}
               components={animatedComponents}
+              value={
+                data.availablePickUpDays.map(day => (
+                  { value: day, label: day }
+                ))
+              }
             />
             <br/>
             <Select 
@@ -169,6 +174,11 @@ class DashboardEdit extends React.Component {
               isMulti
               onChange={this.handleTimeSelect}
               components={animatedComponents}
+              value={
+                data.availablePickUpTimes.map(time => (
+                  { value: time, label: time + ':00' }
+                ))
+              }
             />
             <button onClick={this.handleSubmit}>Submit</button>
           </div>
